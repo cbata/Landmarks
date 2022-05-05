02882ad52a4a7d41f29f43778b7808505ac1a0f8
Chris Bata <cjbata@gmail.com>
2022-05-05 09:04:11 -0600

    Hook up app to data source

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,54 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Chris Bata on 5/5/22.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+
+                Text("Description text goes here")
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -5,4 +5,31 @@
 //  Created by Chris Bata on 5/5/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+
+    }
+}
+
